42550a05b32b9dde33e000da4577af5df36c944c
Marius Grigore <mariusgrygore@gmail.com>
2021-05-09 03:43:01 +0100

    Created and exported page components

---
+++ src/pages/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <p>
+      This is the NavBar page.
+    </p>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export {default as About} from './About/About';
+export {default as Contact} from './Contact/Contact';
+export {default as Home} from './Home/Home';
+export {default as NavBar} from './NavBar/NavBar';
+export {default as NotFound} from './NotFound/NotFound';
+export {default as Portfolio} from './Portfolio/Portfolio';
+export {default as Work} from './Work/Work';
